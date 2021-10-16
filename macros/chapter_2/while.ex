02688ab7c9b242d6feff8d9c426307379a3db656
Alexey Ivanov <alexeyivanov@yandex.ru>
2021-10-16 22:36:46 +0000

    Make not macro while version

--- macros/chapter_2/while.ex
+++ macros/chapter_2/while.ex
@@ -16,4 +16,11 @@ defmodule While do
   end
 
   def break, do: throw :break
+
+  def not_macro_while(expression, do_block) do
+    if expression.() do
+      do_block.()
+      not_macro_while(expression, do_block)
+    end
+  end
 end
