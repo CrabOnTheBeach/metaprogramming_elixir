804104bd064dd9db73d13bc4d4b47568942c54c2
Alexey Ivanov <alexeyivanov1861@yandex.ru>
2022-01-04 23:24:54 +0200

    Add more compare operation to test framework, refactor

--- macros/chapter_2/assertion/assertion/test.ex
+++ macros/chapter_2/assertion/assertion/test.ex
@@ -1,10 +1,11 @@
 defmodule Assertion.Test do
+  @operations [:==, :!=, :>, :>=, :<, :<=]
+
   def run(tests, module) do
     Enum.each(tests, fn {test_func, description} ->
       case apply(module, test_func, []) do
         :ok             -> IO.puts("OK")
         {:fail, reason} -> IO.write(
-          #TODO: change """ with sigil
           """
           ===================================================
           FAILURE: #{description}
@@ -15,28 +16,31 @@ defmodule Assertion.Test do
     end)
   end
 
-  def assert(:==, left, right) when left == right do
-    :ok
+  def assert(:==, left, right) when left == right, do: :ok
+  def assert(:!=, left, right) when left != right, do: :ok
+  def assert(:>, left, right) when left > right, do: :ok
+  def assert(:>=, left, right) when left >= right, do: :ok
+  def assert(:<, left, right) when left < right, do: :ok
+  def assert(:<=, left, right) when left <= right, do: :ok
+  def assert(operation, left, right) when operation in @operations do
+    {:fail, error_message(operation, left, right)}
   end
-  def assert(:==, left, right) do
-    #TODO change """ with sigil
-    reason =
-      """
-      Expected left:        #{inspect(left)}
-      to be equal to right: #{inspect(right)}
-      """
-    {:fail, reason}
+
+  defp error_message(operation, left, right) when operation in @operations do
+    """
+    Expecter left: #{inspect(left)}
+    To be #{inspect(relationship(operation))} right: #{inspect(right)}
+    """
   end
-  def assert(:>, left, right) when left > right do
-    :ok
+  defp error_message(operation, _, _) do
+    "Not supported assert operation: #{inspect(operation)}"
   end
-  def assert(:>, left, right) do
-    #TODO change """ with sigil
-    reason =
-      """
-      Expected left:        #{inspect(left)}
-      to be equal to right: #{inspect(right)}
-      """
-    {:fail, reason}
-  end
+
+  defp relationship(:==), do: "equal to"
+  defp relationship(:!=), do: "not equal to"
+  defp relationship(:< ), do: "less than"
+  defp relationship(:<=), do: "not greater than"
+  defp relationship(:> ), do: "grater than"
+  defp relationship(:>=), do: "not less than"
 end
+
